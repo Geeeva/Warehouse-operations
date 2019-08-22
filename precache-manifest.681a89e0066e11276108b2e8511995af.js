431ccc1154c916b02de27c87bbc0b76cbf47f2ce
Geeeva <ivana.gilich@gmail.com>
2019-08-22 20:26:22 +0200

    Updates

---
+++ precache-manifest.681a89e0066e11276108b2e8511995af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc3cd0189528258578d1f6cd8e3a28f",
+    "url": "/Warehouse-operations/index.html"
+  },
+  {
+    "revision": "1d59def087595fb79b6f",
+    "url": "/Warehouse-operations/static/css/main.7c5e62c9.chunk.css"
+  },
+  {
+    "revision": "fdfae65f9a9b4f8fc973",
+    "url": "/Warehouse-operations/static/js/2.bebe509d.chunk.js"
+  },
+  {
+    "revision": "1d59def087595fb79b6f",
+    "url": "/Warehouse-operations/static/js/main.c2d3ec40.chunk.js"
+  },
+  {
+    "revision": "1b5e6aec3705882c51fb",
+    "url": "/Warehouse-operations/static/js/runtime~main.9a02129b.js"
+  },
+  {
+    "revision": "fc84e998bc29b297ea20321e4c90b6ed",
+    "url": "/Warehouse-operations/static/media/Roboto-Light.fc84e998.ttf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/Warehouse-operations/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "f0466ca59c8c9e0321c3eaf2541bf57e",
+    "url": "/Warehouse-operations/static/media/cover.f0466ca5.png"
+  }
+]);
